3d23c70071ba8217504b0d1048908a4462b53b3f
Ajmal Shajahan <23806715+AjmalShajahan@users.noreply.github.com>
2025-08-13 23:34:24 +0530

    chore: bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.nutomic.syncthingandroid"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4395
-        versionName = "1.28.1"
+        versionCode = 4396
+        versionName = "2.0.0"
         testApplicationId = "com.nutomic.syncthingandroid.test"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
